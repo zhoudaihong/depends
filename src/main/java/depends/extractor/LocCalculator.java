aea3e08e943a3199edf02171cbcdb7d168b81393
lzl <1104609515@qq.com>
2020-10-22 21:51:44 +0800

    修复计算代码有效行数函数的无法计算单独空行文件的错误。

--- src/main/java/depends/extractor/LocCalculator.java
+++ src/main/java/depends/extractor/LocCalculator.java
@@ -5,7 +5,7 @@ public class LocCalculator {
     public static int calcLoc(String codeLine) {
         codeLine = codeLine.replaceAll("//[\\s\\S]*?\\n", "");
         codeLine = codeLine.replaceAll("/\\*{1,2}[\\s\\S]*?\\*/", "");
-        codeLine = codeLine.replaceAll("((\r\n)|\n)[\\s\t]*(\\1)+", "\n");
+        codeLine = codeLine.replaceAll("((\r\n)|\n)[\\s\t]*(\\1)*", "\n");
         codeLine = codeLine.replaceAll("^\\n", "");
         String validCodeLine = codeLine.replaceAll("\n","");
         return codeLine.length()-validCodeLine.length();
